ce56c11fea90a72a328a05f6132c446af8487f29
nik27 <nik27@protonmail.com>
2020-07-21 21:53:47 +0200

    ✨ Notification handling for recipe likes

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,17 @@ const app = require('express')()
 
 const functions = require('firebase-functions')
 
+const { db } = require('./utils/admin')
 const firebaseAuthorizationMiddleware = require('./utils/authMiddleware')
-const { handleSignUp, handleLogin, uploadProfilePicture, createDetails, getDetails } = require('./handlers/user')
+
+const {
+  handleSignUp,
+  handleLogin,
+  uploadProfilePicture,
+  createDetails,
+  getCurrentUserDetails
+} = require('./handlers/user')
+
 const {
   getAllRecipes,
   getRecipe,
@@ -23,7 +32,7 @@ app.post('/login', handleLogin)
 // User profile routes
 app.post('/user/profile-picture', firebaseAuthorizationMiddleware, uploadProfilePicture)
 app.post('/user/details', firebaseAuthorizationMiddleware, createDetails)
-app.get('/user/details', firebaseAuthorizationMiddleware, getDetails)
+app.get('/user/details', firebaseAuthorizationMiddleware, getCurrentUserDetails)
 
 // Recipe routes
 app.get('/recipe', firebaseAuthorizationMiddleware, getAllRecipes)
@@ -39,3 +48,28 @@ app.get('/recipe/:id/remove-bookmark', firebaseAuthorizationMiddleware, removeBo
 app.post('/recipe/:id/comment', firebaseAuthorizationMiddleware, createComment)
 
 exports.api = functions.region('europe-west3').https.onRequest(app)
+
+exports.onLikeNotification = functions.region('europe-west3').firestore.document('likes/{id}')
+  .onCreate(querySnapshot => {
+    db.doc(`/recipes/${querySnapshot.data().recipeId}`).get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${querySnapshot.id}`).set({
+            recipeId: doc.id,
+            type: 'like',
+            read: false,
+            sender: querySnapshot.data().userHandle,
+            recipient: doc.data().userHandle,
+            createdAt: new Date().toISOString()
+          })
+        }
+      })
+      .catch(err => { console.log(err) })
+  })
+
+exports.onUnlikeRemoveNotification = functions.region('europe-west3').firestore.document('likes/{id}')
+  .onDelete(querySnapshot => {
+    db.doc(`/notifications/${querySnapshot.id}`)
+      .delete()
+      .catch(err => { console.log(err) })
+  })
