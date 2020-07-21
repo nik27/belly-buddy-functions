41364f6892e4af4fc0de39d8ed2c3dd5e9015ef9
nik27 <nik27@protonmail.com>
2020-07-21 14:18:23 +0200

    ✨ Add authorization middleware

---
+++ functions/utils/authMiddleware.js
@@ -0,0 +1,23 @@
+const { admin, db } = require('../utils/admin')
+
+module.exports = (req, res, next) => {
+  let token
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    token = req.headers.authorization.split('Bearer ')[1]
+  } else {
+    return res.status(403).json({ error: 'Unauthorized' })
+  }
+
+  admin.auth().verifyIdToken(token)
+    .then(decodedToken => {
+      req.user = decodedToken
+      return db.collection('users').where('userId', '==', req.user.uid).limit(1).get()
+    })
+    .then(querySnapshot => {
+      req.user.handle = querySnapshot.docs[0].data().handle
+      req.user.imgUrl = querySnapshot.docs[0].data().imgUrl
+      return next()
+    })
+    .catch(err => (res.status(403).json(err)))
+}
